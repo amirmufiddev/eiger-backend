dd1f1e1c0faeb37c026098c597d5a252dd2b3af4
Amir Mufid <amir.mufid@logique.co.id>
2026-05-02 00:49:43 +0700

    clean

--- src/modules/realtime/realtime.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GateGateway } from './gate.gateway';
-
-@Module({
-  providers: [GateGateway],
-  exports: [GateGateway],
-})
-export class RealtimeModule {}
